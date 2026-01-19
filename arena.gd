e3a7aa7168ef31bccc45abe0c82e64d8dc87110f
OwenDdev <porewen3848@conestogac.on.ca>
2026-01-19 14:19:45 -0500

    enemy spawner
    Enemy spawning implemented

--- arena.gd
+++ arena.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var enemy_1 = preload("res://enemy.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,3 +8,11 @@ func _ready() -> void:
 	
 func _exit_tree() -> void:
 	Global.node_creation_parent = null
+	
+func _on_enemy_spawn_timer_timeout() -> void:
+	var enemy_position = Vector2(randf_range(-160, 670), randf_range(-90, 390))
+	
+	while enemy_position.x < 640 and enemy_position.x > -80 or enemy_position.x < 360 and enemy_position.y > -45:
+		enemy_position = Vector2(randf_range(-160, 670), randf_range(-90, 390))
+	
+	Global.instance_node(enemy_1, enemy_position, self)
